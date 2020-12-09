b7347b6de7b419e16a5c54e48863f42bda777a71
Christophedlr <christophedlr@gmail.com>
2020-12-09 17:16:33 +0100

    Add update password

--- bank/bank/app/forms/profile.py
+++ bank/bank/app/forms/profile.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import MinLengthValidator, RegexValidator, ValidationError
 
 
 # Custom login form
@@ -10,3 +11,48 @@ class ProfileForm(forms.Form):
         self.fields['email'].widget.attrs.update(
             {'class': 'form-control', 'maxlength': '254'}
         )
+
+
+class ChangePasswordForm(forms.Form):
+    password1 = forms.CharField(
+        max_length="128",
+        required=True,
+        label="Nouveau mot de passe",
+        help_text="Le mot de passe doit faire 8 caractères minimum et, comporter des lettres & des chiffres",
+        validators=[
+            MinLengthValidator(8, "Votre mot de passe doit faire au moins 8 caractères"),
+            RegexValidator('^(?=.*[\\d])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$',
+                           'Vous devez mélanger des lettres et des chiffres'),
+        ],
+        widget=forms.PasswordInput()
+    )
+
+    password2 = forms.CharField(
+        max_length="128",
+        required=True,
+        label="Confirmation du nouveau mot de passe",
+        validators=[
+            MinLengthValidator(8, "Votre mot de passe doit faire au moins 8 caractères"),
+            RegexValidator('^(?=.*[\\d])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$',
+                           'Vous devez mélanger des lettres et des chiffres'),
+        ],
+        widget=forms.PasswordInput()
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ChangePasswordForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget.attrs.update(
+            {'class': 'form-control', 'maxlength': '128'}
+        ),
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'maxlength': '128'}
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 != password2:
+            self.add_error('password1', ValidationError('Les deux mots de passe ne correspondent pas'))
--- bank/bank/app/views.py
+++ bank/bank/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import update_session_auth_hash
 
 from bank.app.forms.register import RegisterForm
-from bank.app.forms.profile import ProfileForm
+from bank.app.forms.profile import ProfileForm, ChangePasswordForm
 
 
 def index(request):
@@ -33,8 +33,9 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST)
+        form2 = ChangePasswordForm(request.POST)
 
-        if form.is_valid():
+        if "email_form" in request.POST and form.is_valid():
             email = form.cleaned_data.get('email')
 
             if email != request.user.email:
@@ -45,7 +46,20 @@ def profile(request):
                 update_session_auth_hash(request, user)
 
                 return redirect('index')
+        elif "password_form" in request.POST and form2.is_valid():
+            password1 = form2.cleaned_data.get('password1')
+            password2 = form2.cleaned_data.get('password2')
+
+            if password1 == password2:
+                user = request.user
+                user.set_password(password1)
+
+                user.save()
+                update_session_auth_hash(request, user)
+
+                return redirect('index')
     else:
         form = ProfileForm()
+        form2 = ChangePasswordForm()
 
-    return render(request, 'app/user/profile.html', {'form': form, 'request': request})
+    return render(request, 'app/user/profile.html', {'form': form, 'form2': form2, 'request': request})
