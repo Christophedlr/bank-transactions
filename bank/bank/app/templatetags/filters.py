b9e03ea9b80ad63b4b7871535b34527be84efd17
Christophedlr <christophedlr@gmail.com>
2020-12-10 17:06:47 +0100

    Fix bug

--- bank/bank/app/templatetags/filters.py
+++ bank/bank/app/templatetags/filters.py
@@ -7,7 +7,7 @@ register = template.Library()
 # Adding a class attribute
 @register.filter(is_safe=True)
 def add_class(value, arg):
-    classes = value.subwidgets[0].data['attrs']['class'] + ' ' + arg
+    classes = value.subwidgets[0].data['attrs'].get('class', '') + ' ' + arg
 
     value = copy(value)
     return value.as_widget(attrs={'class': classes})
