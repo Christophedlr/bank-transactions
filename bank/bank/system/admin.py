de50876aaa46a7c093f5e8c5bd990da57f19ef3d
Christophedlr <christophedlr@gmail.com>
2020-12-14 09:25:43 +0100

    Add admin Django system tables

--- bank/bank/system/admin.py
+++ bank/bank/system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from bank.system.models import Category, Transaction, Account
+
+admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(Account)
