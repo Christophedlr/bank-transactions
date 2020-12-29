52a3cf9608a56331ffc6d341b5c52e26122598b9
Christophedlr <christophedlr@gmail.com>
2020-12-29 14:33:45 +0100

    Fix close modal dialog

--- bank/bank/system/static/system/js/modal.js
+++ bank/bank/system/static/system/js/modal.js
@@ -1,20 +1,22 @@
-$.each($('[data-toggle="modal"]'), function() {
-    target = $(this).data('target');
+$(document).ready(function() {
+    $.each($('[data-toggle="modal"]'), function() {
+        target = $(this).data('target');
 
-    if ($(target).hasClass("modal")) {
-        $(target).dialog({
-            autoOpen: false,
-            modal: true
-        });
-        $('.ui-dialog-titlebar').remove();
+        if ($(target).hasClass("modal")) {
+            $(target).dialog({
+                autoOpen: false,
+                modal: true
+            });
+            $('.ui-dialog-titlebar').remove();
 
-        $(target).find('[data-dismiss="modal"]').on('click', function() {
-            $('.modal').dialog('close');
-        });
-    }
-});
+            $(target).find('[data-dismiss="modal"]').on('click', function() {
+                $('.modal').closest('.ui-dialog-content').dialog('close');
+            });
+        }
+    });
 
-$('[data-toggle="modal"]').click(function(event) {
-    event.preventDefault();
-    $($(this).data('target')).dialog("open");
+    $('[data-toggle="modal"]').click(function(event) {
+        event.preventDefault();
+        $($(this).data('target')).dialog("open");
+    });
 });
