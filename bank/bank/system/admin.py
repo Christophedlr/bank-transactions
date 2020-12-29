f1f5fce499a8e906dac3c37d094625f0e5bec933
Christophedlr <christophedlr@gmail.com>
2020-12-29 14:34:37 +0100

    Custom display admin list account

--- bank/bank/system/admin.py
+++ bank/bank/system/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from bank.system.models import Category, Transaction, Account
 
+
+class AccountAdmin(admin.ModelAdmin):
+    model = Account
+
+    list_display = ('name', 'currency', 'decimal')
+
+
 admin.site.register(Category)
 admin.site.register(Transaction)
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
