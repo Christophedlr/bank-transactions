748b2d03f14df821db21d9c1d4685d644b0ab457
Christophedlr <christophedlr@gmail.com>
2022-01-24 22:07:14 +0100

    AbstractAdministrationLinks abstract class
    Signed-off-by: Christophedlr <christophedlr@gmail.com>

--- bank/bank/administration/admin.py
+++ bank/bank/administration/admin.py
@@ -1,3 +1,40 @@
+from abc import ABC
 from django.contrib import admin
 
-# Register your models here.
+
+class AbstractAdministrationLinks(ABC):
+    """
+    Managing administration menus
+    """
+
+    menu: dict = {}
+
+    def add_menu(self, name: str):
+        """
+        Adding new menu for administration links.
+
+        :param name: A string name of menu.
+        """
+        self.menu[name] = []
+
+    def add_link(self, menu_name: str, name: str, url_resolver: str):
+        """
+        Adding new link in selected menu.
+
+        :param menu_name: A string name of menu
+        :param name: A string name of link
+        :param url_resolver: A string name of url defined in urls.py
+        """
+        if menu_name in self.menu:
+            self.menu[menu_name].append({
+                'name': name,
+                'url': url_resolver,
+            })
+
+    def get(self):
+        """
+        Get all administration menu.
+
+        :return: A dict of menu
+        """
+        return self.menu
